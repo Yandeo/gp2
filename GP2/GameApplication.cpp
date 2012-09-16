4e8aa946e2e63df6c8d603c8d0ff5db72bf9e7d9
Andrew John Lindsay <yandeo@hotmail.com>
2012-09-16 16:43:19 +0100

    Finished until tonight

--- GP2/GameApplication.cpp
+++ GP2/GameApplication.cpp
@@ -3,14 +3,14 @@
 struct Vertex
 {
 	D3DXVECTOR3 Pos;
-}
-
+};
 CGameApplication::CGameApplication(void)
 {
 	m_pWindow-NULL;
 	m_pD3D10Device=NULL;
 	m_pRenderTargetView=NULL;
 	m_pSwapChain=NULL;
+	m_pVertextBuffer=NULL;
 }
 
 CGameApplication::~CGameApplication(void)
